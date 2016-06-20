42983c03ea02df8c8d99f81a961d0037242f15a9
Travis <travis@travis-ci.org>
2016-06-20 13:15:40 -0500

    o

---
+++ cache/smarty/compile/17/90/b3/1790b35067bc372055e9c8daf3892cfa1cd80a6b.file.list.tpl.php
@@ -0,0 +1,35 @@
+<?php /* Smarty version Smarty-3.1.19, created on 2016-06-20 12:44:42
+         compiled from "C:\xampp\htdocs\joyeria\admin631gsbait\themes\default\template\helpers\list\list.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:3037557682b8a98c504-34362728%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '1790b35067bc372055e9c8daf3892cfa1cd80a6b' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\joyeria\\admin631gsbait\\themes\\default\\template\\helpers\\list\\list.tpl',
+      1 => 1460131476,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '3037557682b8a98c504-34362728',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'header' => 0,
+    'content' => 0,
+    'footer' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.19',
+  'unifunc' => 'content_57682b8a994203_18080201',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_57682b8a994203_18080201')) {function content_57682b8a994203_18080201($_smarty_tpl) {?>
+<?php echo $_smarty_tpl->tpl_vars['header']->value;?>
+
+<?php echo $_smarty_tpl->tpl_vars['content']->value;?>
+
+<?php echo $_smarty_tpl->tpl_vars['footer']->value;?>
+
+<?php }} ?>
