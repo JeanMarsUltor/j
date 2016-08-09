c78289ff64000fe0bf43b113dfd32d7a4ac4f30f
Travis <travis@travis-ci.org>
2016-08-09 13:13:23 -0500

    new changes

--- gitcommit.bat
+++ gitcommit.bat
@@ -1,9 +1,9 @@
 cd C:\xampp\htdocs\joyeria
 mysqldump -u root silverzum > silverzum.sql
 copy silverzum.sql silverzum_web.sql
-fart -- "silverzum.sql" "localhost:8888" "www.silverzum.com"
-fart -- "silverzum.sql" "/joyeria" "/"
-fart -- "silverzum.sql" "//" "/"
+fart -- "silverzum_web.sql" "localhost:8888" "www.silverzum.com"
+fart -- "silverzum_web.sql" "/joyeria" "/"
+fart -- "silverzum_web.sql" "//" "/"
 git add --all
 git commit -m "new changes"
 git push
